f78a87e5fc2ed849bfae5d90be26eed87b5b0164
Skycocoo <luoyx2014@163.com>
2019-05-06 23:26:37 -0400

    Use dagre.js to graph tree

--- js/index.js
+++ js/index.js
@@ -1,12 +1,24 @@
 var cy = cytoscape({
     container: $('#cy'),
     style: [
-    {
-        selector: 'node',
-        style: {
-          'label': 'data(id)'
-        }
-    }
+        {
+            selector: 'node',
+            style: {
+                'label': 'data(id)',
+                'text-valign': 'center',
+                'text-halign': 'right',
+            }
+        },
+        {
+            selector: 'edge',
+            style: {
+                'curve-style': 'bezier',
+                // 'width': 4,
+                // 'target-arrow-shape': 'triangle',
+                // 'line-color': '#9dbaea',
+                // 'target-arrow-color': '#9dbaea'
+            }
+        },
     ],
     userZoomingEnabled: false,
 });
@@ -14,28 +26,35 @@ var cy = cytoscape({
 var elems = [];
 
 for (i = 0; i < 5; ++i) {
-    var node = {
+    elems.push({
         group: 'nodes',
         data: {
             id: i.toString(10),
         }
-    };
-    elems.push(node);
+    });
 
-    if (i == 4) {
-        break;
+    if (i > 1) {
+        elems.push({
+            group: 'edges',
+            data: {
+                id: 'e' + (i-2).toString(10),
+                source: (i-2).toString(10),
+                target: (i).toString(10),
+            }
+        });
     }
 
-    var edge = {
-        group: 'edges',
-        data: {
-            id: 'e' + (i+1).toString(10),
-            source: (i+1).toString(10),
-            target: (i).toString(10),
-        },
-    };
-    elems.push(edge);
+    if (i < 4) {
+        elems.push({
+            group: 'edges',
+            data: {
+                id: 'e' + (i+1).toString(10),
+                source: (i+1).toString(10),
+                target: (i).toString(10),
+            },
+        });
+    }
 }
 cy.add(elems);
 
-cy.layout({ name: 'grid' }).run();
+cy.layout({ name: 'dagre' }).run();
