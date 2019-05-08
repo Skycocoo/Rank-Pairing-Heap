f1096d3a7a4bdb2f7491272015fde0dbc48a6ddd
Skycocoo <luoyx2014@163.com>
2019-05-07 21:55:18 -0400

    Finish insertion

--- js/index.js
+++ js/index.js
@@ -26,97 +26,90 @@ function init_cy(cont) {
 }
 
 function calc_rank_cy(cy, arr, arrid) {
-    for (i = arr.length - 1; i >= 0; --i) {
-        if (arr[i] != null) {
-            let cur_id = arrid + arr[i];
-            let left = 2 * i + 1;
-            let right = 2 * i + 2;
-            let left_rank = -1, right_rank = -1;
+    for (iter = arr.length - 1; iter >= 0; --iter) {
+        if (arr[iter] == null) continue;
+        let cur_id = arrid + arr[iter],
+            left = 2 * iter + 1,
+            right = 2 * iter + 2,
+            left_rank = -1,
+            right_rank = -1;
 
-            if (left < arr.length && arr[left] != null) {
-                left_rank = cy.$id(arrid + arr[left]).data()['rank'];
-            }
-            if (right < arr.length && arr[right] != null) {
-                right_rank = cy.$id(arrid + arr[right]).data()['rank'];
-            }
+        if (left < arr.length && arr[left] != null) {
+            left_rank = cy.$id(arrid + arr[left]).data()['rank'];
+        }
+        if (right < arr.length && arr[right] != null) {
+            right_rank = cy.$id(arrid + arr[right]).data()['rank'];
+        }
 
-            if (i == 0) {
-                cy.$id(cur_id).data('rank', left_rank + 1);
-            } else if (Math.abs(left_rank - right_rank) <= 1) {
-                cy.$id(cur_id).data('rank', Math.max(left_rank, right_rank) + 1);
-            } else {
-                cy.$id(cur_id).data('rank', Math.max(left_rank, right_rank));
-            }
+        if (iter == 0) {
+            cy.$id(cur_id).data('rank', left_rank + 1);
+        } else if (Math.abs(left_rank - right_rank) <= 1) {
+            cy.$id(cur_id).data('rank', Math.max(left_rank, right_rank) + 1);
+        } else {
+            cy.$id(cur_id).data('rank', Math.max(left_rank, right_rank));
         }
     }
 }
 
 function add_tree_cy(cy, arr, arrid) {
-    let level = Math.ceil(Math.log(arr.length));
-    let last_level = Math.pow(2, level) - 1;
-
-    // add nodes to the graph
-    let elems = [];
+    // console.log(arr);
     for (i = 0; i < arr.length; ++i) {
-        if (arr[i] != null) {
-            elems.push({
-                group: 'nodes',
-                data: {
-                    id: arrid + arr[i],
-                    val: arr[i],
-                    rank: -1,
-                },
-            });
+        let elems = [];
+        for (j = 0; j < arr[i].length; ++j) {
+            // console.log(arr[i][j]);
+            if (arr[i][j] != null) {
+                elems.push({
+                    group: 'nodes',
+                    data: {
+                        id: arrid + i + arr[i][j],
+                        val: arr[i][j],
+                        rank: -1,
+                    },
+                });
+            }
         }
-    }
-    cy.add(elems);
+        cy.add(elems);
 
-    elems = [];
-    for (i = arr.length - 1; i >= 0; --i) {
-        if (arr[i] != null) {
-            let cur_id = arrid + arr[i];
-            let left = 2 * i + 1;
-            let right = 2 * i + 2;
+        elems = [];
+        for (j = arr[i].length-1; j >= 0; --j) {
+            if (arr[i][j] == null) continue;
+            let cur_id = arrid + i + arr[i][j];
+            let left = 2 * j + 1;
+            let right = 2 * j + 2;
             let left_rank = -1, right_rank = -1;
 
-            if (left < arr.length && arr[left] != null) {
+            if (left < arr[i].length && arr[i][left] != null) {
                 elems.push({
                     group: 'edges',
                     data: {
                         source: cur_id,
-                        target: arrid + arr[left],
+                        target: arrid + i + arr[i][left],
                     }
                 });
             }
-            if (right < arr.length && arr[right] != null) {
+            if (right < arr[i].length && arr[i][right] != null) {
                 elems.push({
                     group: 'edges',
                     data: {
                         source: cur_id,
-                        target: arrid + arr[right],
+                        target: arrid + i + arr[i][right],
                     }
                 });
             }
         }
+        cy.add(elems);
+        calc_rank_cy(cy, arr[i], arrid+i);
     }
-    cy.add(elems);
-
-    calc_rank_cy(cy, arr, arrid);
-
     return cy;
 }
 
-function display_tree_cy(cy) {
+function display_tree_cy(cy, label) {
     cy.nodeHtmlLabel([
         {
             query: 'node',
             halign: 'right',
             valign: 'center',
-            tpl: function (data) {
-                // return 'Val: ' + data.id + '\nRank:' + data.rank;
-                return '<p class="cyid"> Val: ' + data.val + '</p>' +
-                       '<p class="cyrank"> Rank: ' + data.rank + '</p>';
-            }
+            tpl: label,
         },
     ]);
 
@@ -124,26 +117,71 @@ function display_tree_cy(cy) {
 }
 
 
-// let cy = init_cy($('#cy'));
+class RankPairingHeap {
+    constructor() {
+        this.arr = [];
+    }
+    insert(arr) {
+        if (this.arr.length == 0) {
+            this.arr.push(arr);
+        } else {
+            if (this.arr[0][0] < arr[0]) {
+                this.arr.push(arr);
+            } else {
+                this.arr.unshift(arr);
+            }
+        }
+        console.log(this.arr);
+    }
+}
 
-let arr = [1,
+let rp = new RankPairingHeap();
+let heap = [1,
             2, 3,
             4, 6, 7, 8,
             5, 10, null, null, null, null, null, null,
             9, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null];
+
 let half = [1,
             2, null,
             4, 3, null, null,
             5, 6, 7, 8, null, null, null, null,
             9, 10, null, null, null, null, null, null, null, null, null, null, null, null, null, null];
 
+rp.insert(half);
+
 // should add to cy and display tree
 
-let arr_cy = add_tree_cy(init_cy($('#cy-arr')), arr, 'arr');
-let half_cy = add_tree_cy(init_cy($('#cy-half')), half, 'half');
+let heap_cy = add_tree_cy(init_cy($('#cy-heap')), [heap], 'heap');
+let half_cy = add_tree_cy(init_cy($('#cy-half')), rp.arr, 'half');
 
-display_tree_cy(arr_cy);
-display_tree_cy(half_cy);
+display_tree_cy(heap_cy, function (data) {
+    return '<p class="cyid"> Val: ' + data.val + '</p>';
+});
+display_tree_cy(half_cy, function (data) {
+    return '<p class="cyid"> Val: ' + data.val + '</p>';
+});
 
-// should calculate rank based on nodes and edges
-// calc_rank_cy(arr_cy);
+
+let insert_cy = add_tree_cy(init_cy($('#cy-insert')), rp.arr, 'insert');
+display_tree_cy(insert_cy, function (data) {
+    return '<p class="cyid"> Val: ' + data.val + '</p>';
+});
+
+$("#insert-form").submit(function() {
+    if ($("#insert").val() == "") return;
+    rp.insert([parseInt($("#insert").val(), 10)]);
+    insert_cy = add_tree_cy(init_cy($('#cy-insert')), rp.arr, 'insert');
+    display_tree_cy(insert_cy, function (data) {
+        return '<p class="cyid"> Val: ' + data.val + '</p>';
+    });
+    $("#insert").val("");
+    return false;
+});
+
+
+
+
+
+
+//
